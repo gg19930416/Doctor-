00d6cdd6260f8e9e3bb1da136270a39897df80d7
gg19930416 <guogang@yangzhixindeMac-mini.local>
2015-09-23 15:03:53 +0800

    Initial Commit

---
+++ Doctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Doctor
+//
+//  Created by 郭刚 on 15/9/23.
+//  Copyright (c) 2015年 郭刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
